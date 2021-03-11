6e1b9bedad85cf4ba891f31e8632746ba12ef40c
Ram Krishnamoorthy <RAKRISHNAMOORTHY@davita.corp>
2021-03-11 15:15:18 -0700

    First

---
+++ src/test/java/org/openmai/OpenMaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openmai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenMaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
