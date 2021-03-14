d735bc0a0eddeccecf4ed28ae648efb80a781681
Ram Krishnamoorthy <RAKRISHNAMOORTHY@davita.corp>
2021-03-14 04:27:18 -0600

    RequestScope Object For Incoming Request Proptyped

--- src/main/java/org/openmai/OpenMaiApplication.java
+++ src/main/java/org/openmai/OpenMaiApplication.java
@@ -1,7 +1,11 @@
 package org.openmai;
 
+import org.openmai.beans.MAIRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.context.annotation.RequestScope;
 
 @SpringBootApplication
 public class OpenMaiApplication {
--- src/main/java/org/openmai/interceptors/RequestHandler.java
+++ src/main/java/org/openmai/interceptors/RequestHandler.java
@@ -1,14 +1,23 @@
 package org.openmai.interceptors;
 
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
 import org.openmai.beans.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.ContentCachingRequestWrapper;
@@ -18,34 +27,52 @@ import org.springframework.web.util.ContentCachingRequestWrapper;
 public class RequestHandler implements HandlerInterceptor{
 
 
-    //@Resource(name = "maiRequest")
-	MAIRequest maiRequest;
+	@Autowired
+    @Qualifier("maiRequest")
+	private MAIRequest myMai1;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// TODO Auto-generated method stub
+
 		System.out.println("------------preHandle");
         //ContentCachingRequestWrapper requestWrapper = (ContentCachingRequestWrapper) request;
         String path=request.getRequestURI();
         if(!path.contains("ping")) {
         	return true;
         }
-        
+        myMai1.doNothing();
         
 		Cookie[] cookies = request.getCookies();
-		maiRequest = new MAIRequest();
-		maiRequest.initilize(request);
-		//System.out.println(request.getAttribute("requestDispatcherPath"));
-		//System.out.println(request.getPathInfo());
+        Enumeration<String> headerNames = request.getHeaderNames();
+        String headerName , headerContent;
+        Map<String, String> header = new HashMap<String , String>();
+        if (headerNames != null) {
+                while (headerNames.hasMoreElements()) 
+                {
+                    headerName = headerNames.nextElement();
+                    headerContent = request.getHeader(headerName);
+                    //System.out.println(headerName + " : " + headerContent);
+                    header.put(headerName, headerContent);
+                }
+        }
+        myMai1.headers = header;
+        if ("POST".equalsIgnoreCase(request.getMethod())) 
+        {
+            String jsonString = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+			System.out.println(jsonString);
+			JSONObject jsonBody= new JSONObject(jsonString);
+			myMai1.body = jsonBody;
+        }
+
 		
-		String tmp = maiRequest.headers.get("content-type");
+		String tmp = myMai1.headers.get("content-type");
 		
 		PingContentTypeXlation cmd = new PingContentTypeXlation(); 
-		String newPath = cmd.getCommand(maiRequest.headers.get("content-type"));
+		String newPath = cmd.getCommand(myMai1.headers.get("content-type"));
     	String finalPath = path.replace("ping", newPath);
     	request.getRequestDispatcher(finalPath).forward(request,response);
-    	return true;
+    	return false;
 		//return HandlerInterceptor.super.preHandle(requestWrapper, response, handler);
 	}
 
@@ -62,7 +89,7 @@ public class RequestHandler implements HandlerInterceptor{
 			throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("------------afterCompletion");
-		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+		//HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
 	}
 	
 
